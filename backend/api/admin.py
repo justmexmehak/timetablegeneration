46653e3d603ff95d79dfee38d2ae085f19b43607
Mehak Nauman <119690712+justmexmehak@users.noreply.github.com>
2025-01-14 17:43:08 +0000

    Imported Instructor model in admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course, WorkingDay, Constraint
+from .models import Course, WorkingDay, Constraint, Instructor, Room
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
